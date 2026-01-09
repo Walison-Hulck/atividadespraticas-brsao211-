a504ff852971b8a5ff3647ce213457270a986854
Walison-Hulck <walisonalvesdesouza9@gmail.com>
2026-01-09 20:46:28 -0300

    atividade pratica02-questao04

---
+++ atividadepratica02/questao04.py
@@ -0,0 +1,8 @@
+distancia_percorrida = float(input("Digite a distancia percorrida em km: "))
+combustivel_gasto = float(input("Digite o combustivel gasto em litros: "))
+
+consumo_medio = distancia_percorrida / combustivel_gasto
+
+print(f"Consumo medio: {consumo_medio:.2f} km/l")
+print(f"Distancia: {distancia_percorrida: }")
+print(f"combustivel gasto: {combustivel_gasto} L")
