fca95ae498883e4418e2751537c16f73d6332a4b
Walison-Hulck <walisonalvesdesouza9@gmail.com>
2026-01-14 19:50:35 -0300

    atividadepratica04- questao02

---
+++ atividadepratica04/questao01.py
@@ -0,0 +1,14 @@
+while True:
+    numero01 = float(input("Digite o primeiro numero : "))
+    numero02 = float(input("Digite o segundo numero "))
+    operacao = input("Digite a operacao (+, -, *, /): ")
+
+    if operacao == "+":
+        resultado = numero01 + numero02
+    elif operacao == "-":
+        resultado = numero01 - numero02
+
+    elif operacao == "+":
+
+    elif operacao == "/":
+
---
+++ atividadepratica04/questao02.py
@@ -0,0 +1,32 @@
+notas = []
+
+while True:
+    try:
+        entrada = input("Digite uma nota (0 a 10) ou 'fim' para sair: ")
+
+        if entrada.lower() == 'fim':
+            break
+
+        nota = float(entrada)
+
+        if nota < 0 or nota > 10:
+            raise Exception()
+        
+        notas.append(nota)
+
+
+
+
+    except ValueError:
+        print("Voce deve digitar apenas números ")
+    except Exception:
+        print("Nota invalida")
+
+    soma = 0 
+
+    for noto in nostas:
+        soma += nota
+
+    media = soma / len(notas)
+
+    print(f"Media final: {media:.2f} ")
