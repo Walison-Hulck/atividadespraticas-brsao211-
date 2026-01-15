28fa982e327934281c9a656dd5709051b4e387c6
Walison-Hulck <walisonalvesdesouza9@gmail.com>
2026-01-14 21:17:27 -0300

    atividadepratica04-questao01

--- atividadepratica04/questao01.py
+++ atividadepratica04/questao01.py
@@ -1,14 +1,28 @@
 while True:
-    numero01 = float(input("Digite o primeiro numero : "))
-    numero02 = float(input("Digite o segundo numero "))
-    operacao = input("Digite a operacao (+, -, *, /): ")
+    try:
 
-    if operacao == "+":
-        resultado = numero01 + numero02
-    elif operacao == "-":
-        resultado = numero01 - numero02
+        numero01 = float(input("Digite o primeiro numero : "))
+        numero02 = float(input("Digite o segundo numero: "))
+        operacao = input("Digite a operacao (+, -, *, /): ")
 
-    elif operacao == "+":
+        if operacao == "+":
+            resultado = numero01 + numero02
+        elif operacao == "-":
+            resultado = numero01 - numero02
 
-    elif operacao == "/":
-
+        elif operacao == "*":
+            resultado = numero01 * numero02
+    
+        elif operacao == "/":
+            resultado = numero01 / numero02
+        else:
+            raise Exception()
+        print(f"O resultado é {resultado}")
+        break
+    except ValueError:
+        print("Você deve digitar apenas números")
+    except ZeroDivisionError:
+        print("Não é possivel dividir por zero")
+    except Exception:
+        print("Operação inválida")
+        
