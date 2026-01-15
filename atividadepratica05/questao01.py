64ed5fe57a7e169b1f7303e9d570f03aef822b0b
Walison-Hulck <walisonalvesdesouza9@gmail.com>
2026-01-15 19:22:13 -0300

    atividadepratica05-questao01

---
+++ atividadepratica05/questao01.py
@@ -0,0 +1,12 @@
+def calcular_gorjeta(valor_conta, porcentagem_gorjeta):
+    gorjeta = valor_conta * (porcentagem_gorjeta / 100)
+    return gorjeta
+
+
+
+valor = float(input("Digite o valor da conta: "))
+porcentagem = float(input(" Digite a porcentagem da gorjeta: "))
+
+gorjeta = calcular_gorjeta(valor,porcentagem)
+
+print(f"Ovalor da gorjeta é de {gorjeta:.2f}")
