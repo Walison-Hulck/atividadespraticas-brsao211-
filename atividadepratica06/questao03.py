633c599ac480a77125a9abdb1e38f161956a3eac
Walison-Hulck <walisonalvesdesouza9@gmail.com>
2026-01-16 21:37:26 -0300

    atividadepratica06-questao03

--- atividadepratica06/questao03.py
+++ atividadepratica06/questao03.py
@@ -12,4 +12,16 @@ def consultar_cep(cep):
             return "Cep inválido."
         
         logradouro = dados['logradouro']
+        bairro = dados['bairro']
+        localidade = dados['localidade']
+        uf = dados['uf']
+
+        return f"Lougradouro: {logradouro}\nBairro: {bairro}\nLocalidade: {localidade}\nUf: {uf}"
+    except requests.RequestException as e:
+        return f"Erro ao consultar CEP: {e}"
+    
+cep = input("Digite o CEP: ")
+resultado = consultar_cep(cep)
+print(resultado)
+
         
